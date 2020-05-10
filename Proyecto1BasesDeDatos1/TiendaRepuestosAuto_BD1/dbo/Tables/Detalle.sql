28d52225f43981617686aaf2161f3a824c1acf5c
JOQUINYD <54180615+JOQUINYD@users.noreply.github.com>
2020-05-10 15:33:00 -0600

    Se agrego tabla detalle
    Se agrego Tablas:
    Detalle
    
    Se unio:
    Detalle con proveedor, parte y orden
    Orden con Cliente

---
+++ Proyecto1BasesDeDatos1/TiendaRepuestosAuto_BD1/dbo/Tables/Detalle.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[Detalle]
+(
+	[ID_Detalle] INT IDENTITY(0,1) NOT NULL , 
+    [Cantidad] INT NOT NULL, 
+    [ID_Proveedor] INT NOT NULL, 
+    [ID_Parte] INT NOT NULL, 
+    [ID_Orden] INT NOT NULL, 
+    CONSTRAINT [PK_Detalle] PRIMARY KEY ([ID_Detalle]), 
+    CONSTRAINT [FK_Proveedor_Detalle] FOREIGN KEY ([ID_Proveedor]) REFERENCES [Proveedor]([ID_Proveedor]), 
+    CONSTRAINT [FK_Parte_Detalle] FOREIGN KEY ([ID_Parte]) REFERENCES [Parte]([ID_Parte]), 
+    CONSTRAINT [FK_Orden_Detalle] FOREIGN KEY ([ID_Orden]) REFERENCES [Orden]([ID_Orden])
+)
--- Proyecto1BasesDeDatos1/TiendaRepuestosAuto_BD1/dbo/Tables/Orden.sql
+++ Proyecto1BasesDeDatos1/TiendaRepuestosAuto_BD1/dbo/Tables/Orden.sql
@@ -3,8 +3,9 @@
 	[ID_Orden] INT IDENTITY(0,1) NOT NULL, 
     [Fecha] DATE NOT NULL, 
     [IVA] DECIMAL(19, 4) NOT NULL, 
-    CONSTRAINT [PK_Orden] PRIMARY KEY ([ID_Orden])
-
+    [ID_Cliente] INT NOT NULL, 
+    CONSTRAINT [PK_Orden] PRIMARY KEY ([ID_Orden]), 
+    CONSTRAINT [FK_Orden_Cliente] FOREIGN KEY ([ID_Cliente]) REFERENCES [Cliente]([ID_Cliente])
 
 
 )
